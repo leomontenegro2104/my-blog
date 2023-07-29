a7400f6966e46dd11f3609f19431133f335e199b
leomontenegro2104 <eng.leonardomontenegro@gmail.com>
2023-07-28 21:45:20 -0300

    feat: home page

--- my-blog/protected/views/site/index.php
+++ my-blog/protected/views/site/index.php
@@ -1,20 +1,35 @@
 <?php
-/* @var $this SiteController */
+// require(Yii::app()->basePath . '/components/ApiHandler.php');
+function getPostsFromApi() {
+  $url = 'https://jsonplaceholder.typicode.com/posts';
+  $response = file_get_contents($url);
+  return json_decode($response, true);
+}
+$posts = getPostsFromApi();
+$mostRecentPosts = array_slice($posts, 0, 6);
+$chunks = array_chunk($mostRecentPosts, ceil(count($mostRecentPosts) / 2));
 
-$this->pageTitle=Yii::app()->name;
-?>
 
-<h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
+function generateRandomDate() {
+  return date('Y-m-d H:i:s', rand(1262055681, 1674000000));
+}
 
-<p>Congratulations! You have successfully created your Yii application.</p>
 
-<p>You may change the content of this page by modifying the following two files:</p>
-<ul>
-	<li>View file: <code><?php echo __FILE__; ?></code></li>
-	<li>Layout file: <code><?php echo $this->getLayoutFile('main'); ?></code></li>
-</ul>
+echo '<h1>Postagens Recentes</h1>';
+echo '<div style="display: flex;">';
 
-<p>For more details on how to further develop this application, please read
-the <a href="http://www.yiiframework.com/doc/">documentation</a>.
-Feel free to ask in the <a href="http://www.yiiframework.com/forum/">forum</a>,
-should you have any questions.</p>
+foreach ($chunks as $columnPosts) {
+  echo '<div style="flex: 1; margin: 10px;">';
+
+  foreach ($columnPosts as $post) {
+    echo '<h2>' . $post['title'] . '</h2>';
+    echo '<p>' . substr($post['body'], 0, 100) . '...</p>';
+    echo '<p>Author: ' . $post['userId'] . '</p>';
+    echo '<p>Data: ' . generateRandomDate() . '</p>';
+    echo '<p><a href="protected/views/site/pages/view_post.php?id=' . $post['id'] . '">Read more</a></p>';
+  }
+
+  echo '</div>';
+}
+
+echo '</div>';
